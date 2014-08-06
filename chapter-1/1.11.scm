c3e5eada3a99d4c475bc7d3200d0b5b1fe2c56b8
Ryan Linton <ryanlntn@gmail.com>
2014-08-06 11:21:52 -0700

    Fix typo

--- chapter-1/1.11.scm
+++ chapter-1/1.11.scm
@@ -3,8 +3,8 @@
 ;; Recursive function where f(n) = n if n < 3 or
 ;; f(n) = f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3.
 (define (f n)
-  (if ((< n 3)
+  (if (< n 3)
       n
       (+ (f (- n 1)) 
          (* 2 (f (- n 2)))
-         (* 3 (f (- n 3)))))))
+         (* 3 (f (- n 3))))))
