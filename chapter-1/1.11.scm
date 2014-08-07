878b5c5ef9e1bfe2cdf5c997d0c39eb6d89a5670
Ryan Linton <ryanlntn@gmail.com>
2014-08-06 17:11:37 -0700

    Add iterative version of f(n)

--- chapter-1/1.11.scm
+++ chapter-1/1.11.scm
@@ -8,3 +8,11 @@
       (+ (f (- n 1)) 
          (* 2 (f (- n 2)))
          (* 3 (f (- n 3))))))
+
+;; Iterative version
+(define (f2 n)
+  (define (f-iter a b c n)
+    (if (= n 0)
+        a
+        (f-iter b c (+ c (* 2 b) (* 3 a)) (- n 1))))
+  (f-iter 0 1 2 n))
